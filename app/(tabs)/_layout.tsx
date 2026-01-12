1c96b894bfeaf711b3b393b8f471d6310ee38595
Corkit <hashencode@gmail.com>
2026-01-12 23:32:20 +0800

    feat: 修改首页

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,35 @@
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from 'expo-router';
 
-import { HeaderButton } from '../../components/HeaderButton';
-import { TabBarIcon } from '../../components/TabBarIcon';
+import { CalendarFold, PawPrint } from 'lucide-react-native';
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: 'black',
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <HeaderButton />
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: 'black',
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color }) => <PawPrint color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Tab One',
+                    tabBarIcon: ({ color }) => <CalendarFold color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="two"
+                options={{
+                    title: 'Tab Two',
+                    tabBarIcon: ({ color }) => <PawPrint color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,27 @@
-import { Stack } from 'expo-router';
+import { Image, Text, View } from 'react-native';
 
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { DefaultLayout } from '~/components/DefaultLayout';
 
 export default function Home() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
-    </>
-  );
+    return (
+        <DefaultLayout safeAreaViewConfig={{ edges: ['top', 'left', 'right'] }}>
+            {/*<Stack.Screen options={{ title: 'Tab One' }} />*/}
+            {/*<View style={styles.container}>*/}
+            {/*  <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />*/}
+            {/*</View>*/}
+            <View className="p-3">
+                <View>
+                    <Text className="text-2xl font-medium">My Pets</Text>
+                </View>
+                <View className="mt-3 flex gap-x-3">
+                    <Image
+                        className={'h-20 w-20 rounded-full'}
+                        source={{
+                            uri: 'https://cdn.dribbble.com/userupload/45589292/file/f88b8bf99c1e6be04a61b6835adbca2a.png?resize=1504x1128&vertical=center',
+                        }}
+                    />
+                </View>
+            </View>
+        </DefaultLayout>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
