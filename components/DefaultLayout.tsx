800379ec6c417c20076e0d2f48f496eab483b268
Corkit <hashencode@gmail.com>
2026-01-23 18:00:40 +0800

    feat: layout组件的修改

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -1,10 +1,14 @@
-import { PropsWithChildren } from 'react';
+import classNames from 'classnames';
+import { isString } from 'lodash';
+import { PropsWithChildren, ReactNode } from 'react';
 import { ScrollView, View } from 'react-native';
 import { SafeAreaView, SafeAreaViewProps, useSafeAreaInsets } from 'react-native-safe-area-context';
+import LayoutHead from '~/components/LayoutHead';
 
 interface IDefaultLayoutProps extends PropsWithChildren {
     safeAreaViewConfig?: SafeAreaViewProps;
     scrollable?: boolean;
+    head?: ReactNode;
     styles?: {
         safeAreaView?: string;
         safeTop?: string;
@@ -14,15 +18,19 @@ interface IDefaultLayoutProps extends PropsWithChildren {
 }
 
 export const DefaultLayout = (props: IDefaultLayoutProps) => {
-    const { safeAreaViewConfig, scrollable = true, styles = {} } = props;
+    const { head, safeAreaViewConfig, scrollable = true, styles = {} } = props;
     const insets = useSafeAreaInsets();
 
     return (
-        <SafeAreaView className={`bg-[#f7f7f7] ${styles.safeAreaView}`} {...safeAreaViewConfig}>
+        <SafeAreaView className={classNames('bg-[#f7f7f7]', styles.safeAreaView)} {...safeAreaViewConfig}>
             {/*顶部安全区域*/}
-            <View className={`absolute left-0 top-0 w-full ${styles.safeTop}`} style={{ height: insets.top }} />
+            <View
+                className={classNames('absolute left-0 top-0 w-full', { 'bg-white': head }, styles.safeTop)}
+                style={{ height: insets.top }}
+            />
+            {isString(head) ? <LayoutHead title={head} /> : head}
             {/*主体内容*/}
-            <View className={`h-full ${styles.mainContent}`}>
+            <View className={classNames('h-full', styles.mainContent)}>
                 {scrollable ? (
                     <ScrollView overScrollMode="never">{props.children}</ScrollView>
                 ) : (
@@ -30,7 +38,7 @@ export const DefaultLayout = (props: IDefaultLayoutProps) => {
                 )}
             </View>
             {/*底部安全区域*/}
-            <View className={`absolute bottom-0 left-0 w-full ${styles.safeBottom}`} style={{ height: insets.bottom }} />
+            <View className={classNames('absolute bottom-0 left-0 w-full', styles.safeBottom)} style={{ height: insets.bottom }} />
         </SafeAreaView>
     );
 };
